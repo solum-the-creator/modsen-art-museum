46894b121f71014ec62b5ae24909e34e6aa3c1b6
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-08-14 16:17:58 +0300

    feat: create base Layout component

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,16 @@
+import { Header } from '@components/header';
+
+import style from './style.module.scss';
+
+type LayoutProps = {
+    children: React.ReactNode;
+};
+export const Layout = ({ children }: LayoutProps) => (
+    <div className={style.layout}>
+        <Header />
+        <main className={style.main}>{children}</main>
+        <footer>
+            <p>Footer</p>
+        </footer>
+    </div>
+);
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
+import { Layout } from '@components/layout';
 import { UrlPaths } from '@constants/paths';
 import { DetailInfo } from '@pages/detail-info';
 import { Favorites } from '@pages/favorites';
 import { Home } from '@pages/home';
 
 export const AppRoutes = () => (
-    <Routes>
-        <Route path={UrlPaths.HOME} element={<Home />} />
-        <Route path={UrlPaths.FAVORITES} element={<Favorites />} />
-        <Route path={UrlPaths.DETAIL_INFO} element={<DetailInfo />} />
-    </Routes>
+    <Layout>
+        <Routes>
+            <Route path={UrlPaths.HOME} element={<Home />} />
+            <Route path={UrlPaths.FAVORITES} element={<Favorites />} />
+            <Route path={UrlPaths.DETAIL_INFO} element={<DetailInfo />} />
+        </Routes>
+    </Layout>
 );
