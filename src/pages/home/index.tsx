af8919063e0cfef65a884eff7943105e64d8dabe
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-08-15 16:47:39 +0300

    feat: create Home page layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,31 @@
+import { Search } from '@components/search';
+
+import { PictureGrid } from './picture-grid';
+import { PictureList } from './picture-list';
+
 import style from './style.module.scss';
 
 export const Home = () => (
-    <div>
-        <h1 className={style.home}>Home</h1>
+    <div className={style.home}>
+        <section className={style.search_block}>
+            <h1 className={style.title}>
+                Let&apos;s Find Some <span className={style.highlight}>Art</span> Here!
+            </h1>
+            <Search />
+        </section>
+        <section className={style.home_section}>
+            <div className={style.text_block}>
+                <h3 className={style.subtitle}>Topics for you</h3>
+                <h2 className={style.title}>Our special gallery</h2>
+            </div>
+            <PictureList />
+        </section>
+        <section className={style.home_section}>
+            <div className={style.text_block}>
+                <h3 className={style.subtitle}>Here some more</h3>
+                <h2 className={style.title}>Other works for you</h2>
+            </div>
+            <PictureGrid />
+        </section>
     </div>
 );
