dd6342302a88578e439797cfd0219916cbd2f257
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-08-17 13:59:54 +0300

    fix: add keys parameter

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -3,5 +3,4 @@ import { API_IMAGE_BASE_URL } from '@constants/api';
 export const getImageUrl = (imageId?: string) =>
     imageId ? `${API_IMAGE_BASE_URL}/${imageId}/full/400,/0/default.jpg` : '';
 
-export const createFieldsString = <T extends object>() =>
-    (Object.keys({} as T) as Array<keyof T>).join(',');
+export const createFieldsString = <T extends object>(keys: Array<keyof T>) => keys.join(',');
