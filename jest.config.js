8a64ec45fc291742ea20e69b521b1d15ce1e4392
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-08-20 00:14:50 +0300

    chore: add alias for context

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ export default {
         '^@components/(.*)$': '<rootDir>/src/components/$1',
         '^@assets/(.*)$': '<rootDir>/src/assets/$1',
         '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+        '^@context/(.*)$': '<rootDir>/src/context/$1',
         '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
         '^@utils/(.*)$': '<rootDir>/src/utils/$1',
         '^@pages/(.*)$': '<rootDir>/src/pages/$1',
