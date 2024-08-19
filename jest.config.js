9a153b261bdbe5781e0169231cfb351e368a54c1
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-08-19 19:19:42 +0300

    chore: add Jest and customize configurations

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+export default {
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        '\\.svg': '<rootDir>/__mocks__/svg.js',
+        '^@components/(.*)$': '<rootDir>/src/components/$1',
+        '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+        '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+        '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    },
+
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
